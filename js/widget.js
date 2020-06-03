931108fbecf531b84052e77689c16597fc1ba715
Gamygams1234 <gamyburgos@gmail.com>
2020-06-02 17:37:06 -0700

    processing the data

--- js/widget.js
+++ js/widget.js
@@ -5,6 +5,18 @@ xhr.onreadystatechange = function () {
       // this will give us the data ad return an object
       let employees = JSON.parse(xhr.responseText);
       console.log(employees);
+      var statusHTML = '<ul class= "bulleted">';
+      for (let index = 0; index < employees.length; index++) {
+        if (employees[index].inoffice == true) {
+          statusHTML += '<li class="in">';
+        } else {
+          statusHTML += '<li class = "out">';
+        }
+        statusHTML += employees[index].name;
+        statusHTML += "</li>";
+      }
+      statusHTML += "</ul>";
+      document.getElementById("employeeList").innerHTML = statusHTML;
     } else if (xhr.status === 404) {
       // file not found
       alert(xhr.statusText);
