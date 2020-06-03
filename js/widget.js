9c2ef5fda2ad60b7112cd62c369b07082e91aa8c
Gamygams1234 <gamyburgos@gmail.com>
2020-06-02 18:47:07 -0700

    Adding meeting rooms

--- js/widget.js
+++ js/widget.js
@@ -5,6 +5,7 @@ xhr.onreadystatechange = function () {
       // this will give us the data ad return an object
       let employees = JSON.parse(xhr.responseText);
       console.log(employees);
+
       var statusHTML = '<ul class= "bulleted">';
       for (let index = 0; index < employees.length; index++) {
         if (employees[index].inoffice == true) {
@@ -30,3 +31,36 @@ xhr.onreadystatechange = function () {
 xhr.open("GET", "data/employees.json");
 
 xhr.send();
+
+var xhr2 = new XMLHttpRequest();
+xhr2.onreadystatechange = function () {
+  if (xhr2.readyState === 4) {
+    if (xhr2.status === 200) {
+      // this will give us the data ad return an object
+      let rooms = JSON.parse(xhr2.responseText);
+
+      var roomStatusHTML = '<ul class= "rooms">';
+      for (let index = 0; index < rooms.length; index++) {
+        if (rooms[index].available == true) {
+          roomStatusHTML += '<li class="empty">';
+        } else {
+          roomStatusHTML += '<li class = "full">';
+        }
+        roomStatusHTML += rooms[index].room;
+        roomStatusHTML += "</li>";
+      }
+      roomStatusHTML += "</ul>";
+      document.getElementById("roomList").innerHTML = roomStatusHTML;
+    } else if (xhr2.status === 404) {
+      // file not found
+      alert(xhr2.statusText);
+    } else if (xhr2.status === 500) {
+      // there was a problem with the server
+      alert(xhr2.statusText);
+    }
+  }
+};
+
+xhr2.open("GET", "data/rooms.json");
+
+xhr2.send();
